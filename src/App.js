42f01bc3b99778bd080f2ca063814eaa2cbf2355
Dylan Baker <dylanmbaker@yahoo.com>
2022-01-12 01:21:48 -0600

    fixed issue where user wasnt showing up

--- src/App.js
+++ src/App.js
@@ -8,18 +8,17 @@ import './index.css';
 
 class App extends React.Component {
   state = {
-    githubUserData: [],
-    githubUser: ""
+    githubUserData: []
   }
-  // componentDidMount() {
-  //   axios.get(`https://api.github.com/users/dbvker`)
-  //     .then(resp => {
-  //       this.setState({
-  //         ...this.state,
-  //         githubUserData: resp.data,
-  //       });
-  //     })
-  // }
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/dbvker`)
+      .then(resp => {
+        this.setState({
+          ...this.state,
+          githubUserData: resp.data,
+        });
+      })
+  }
 
   handleSearchTerm = (e) => {
     this.setState({
@@ -48,14 +47,8 @@ class App extends React.Component {
         <input onChange={this.handleSearchTerm} type='text' placeholder='GitHub Handle'/>
         <button>Search</button>
       </form>
-      {(this.state.githubUserData.length < 0) ?
-        <>
-          <User githubUserData={this.state.githubUserData} />
-          <FollowerList githubUserData={this.state.githubUserData} />
-        </>
-        : <h1>Search for a user!</h1>
-      }
-      
+      <User githubUserData={this.state.githubUserData} />
+      <FollowerList githubUserData={this.state.githubUserData} />
     </div>);
   }
 }
